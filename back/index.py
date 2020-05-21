3cba973edf2a6ce0f749c95f4714b7043d2c9963
Imran <aliimran@gmail.com>
2020-05-21 21:53:06 +1000

    implement restapi-incomplete

--- back/index.py
+++ back/index.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
+# from flask_restful import Resource, Api
+from main import api
 
-indexgg = Blueprint("gabor", __name__)
+indexgg = Blueprint("gabor", __name__, static_folder=".front/static", template_folder="./front/templates")
 
 
-@indexgg.route("/")
-def index():
-    return "Index.. hahaha"
+# @indexgg.route("/")
+# def index():
+#     return "Index.. hahaha"
+api.add_resource(HelloWorld, '/')
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_restful import Resource, Api
 from admin.second import second
 from back.index import indexgg
 
 app = Flask(__name__)
+api = Api(app)
+
 app.register_blueprint(second, url_prefix="/admin")
 app.register_blueprint(indexgg, url_prefix="/gg")
 
